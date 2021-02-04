0ca829c9df54f962b5785f28537276f633250fb1
mzhan <mzhan@student.42.fr>
2021-02-04 18:29:49 +0100

    atoi changed

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: mzhan <mzhan@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/31 16:53:01 by mzhan             #+#    #+#             */
-/*   Updated: 2021/02/03 21:28:57 by mzhan            ###   ########.fr       */
+/*   Updated: 2021/02/04 18:29:22 by mzhan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,25 +16,24 @@ int	ft_atoi(const char *str)
 {
 	int i;
 	int res;
-	int a;
+	int sign;
 
 	i = 0;
 	res = 0;
-	a = 1;
-	if (!str)
-		res = 0;
+	sign = 1;
 	while (str[i] == '\t' || str[i] == '\n' || str[i] == '\v' ||
 			str[i] == '\f' || str[i] == '\r' || str[i] == ' ')
 		i++;
-	if (str[i] == '-')
-		a = -1;
-	i++;
-	while (str[i] != '\0')
+	if (str[i] == '+' || str[i] == '-')
 	{
-		if (str[i] >= '0' && str[i] <= '9')
-			res = (res * 10) + (str[i++] - '0');
-		else
-			break ;
+		if (str[i] == '-')
+			sign = -1;
+		i++;
 	}
-	return (res * a);
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		res = res * 10 + (str[i++] - '0');
+		i++;
+	}
+	return (res * sign);
 }
